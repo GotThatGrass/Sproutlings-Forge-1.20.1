36e7f278dbf8522d914f01464cbd84072686ba68
GotThatGrass <imallaboutthatgrass@gmail.com>
2024-12-15 09:31:41 -0800

    Minor Refinement

--- src/main/java/net/danie/sproutlings/Sproutlings.java
+++ src/main/java/net/danie/sproutlings/Sproutlings.java
@@ -18,9 +18,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Sproutlings.MOD_ID)
 public class Sproutlings {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "sproutlings";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public Sproutlings() {
